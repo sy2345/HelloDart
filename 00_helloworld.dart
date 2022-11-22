6249b2757b1bac00f8b1534aed63e876ff599125
sy2345 <3103791500@qq.com>
2022-11-22 04:52:26 +0000

    hello world

--- 00_helloworld.dart
+++ 00_helloworld.dart
@@ -1,3 +1,9 @@
 void main() {
   print('Hello, World!');
+
+  int a = 20;
+  int b = 40;
+  int c = a + b;
+
+  print('a + b =${c}');
 }
---
+++ 01_dart_lang_variables.dart
@@ -0,0 +1,20 @@
+void name_var(){
+  var name = 'Bob';
+  print('value of name is ${name}');
+}
+
+void name_object(){
+  var name = 'Tim';
+  print('value of object name is ${name}');
+}
+
+void name_string(){
+  var name = 'Sam';
+  print('value of string name is ${name}');
+}
+
+void main(){
+  name_var();
+  name_object();
+  name_string();
+}
